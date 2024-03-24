defe00c4a512da1daabe1a0bea7d94b2acf51527
Dylan <79404455+DylanBracke@users.noreply.github.com>
2024-03-24 13:24:53 +0100

    Added mixins and css
    Added (s)css for buttons, link and headings

--- artdefine/src/pages/Listpage.tsx
+++ artdefine/src/pages/Listpage.tsx
@@ -9,6 +9,11 @@ export default function ListPage() {
         <>
             <h1>Dit is een lijst</h1>
            <List items={items} />
+           <button>Toevoegen</button>
+           <button className="pirmary">Toevoegen</button>
+           <button className="secondary">Toevoegen</button>
+           <button className="has-icon">Toevoegen <i className="material-icons">add</i></button>
+           <div className="link"> <a href="http://">link</a></div>
         </>
     );
 }
