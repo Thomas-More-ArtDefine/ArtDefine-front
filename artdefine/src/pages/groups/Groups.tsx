bafddd266f534ddb55183d01b9f1f2fc8c7471f6
Charlotte-Overeem <r0781396@student.thomasmore.be>
2024-04-17 13:47:23 +0200

    added: group cards & tab change

--- artdefine/src/pages/groups/Groups.tsx
+++ artdefine/src/pages/groups/Groups.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import CreateGroupModal from "../../components/Create-group-modal";
 import Tabs from "../../components/Tabs";
+import GroupCard from "../../components/group/GroupCard";
+import GROUPMOCK from "../../mock/GroupMock";
 
 export default function Groups() {
     const [openModal, setOpenModal] = useState<boolean>(false);
@@ -9,14 +11,14 @@ export default function Groups() {
     const handleCreateClick = () => {
         setOpenModal(true);
     };
+
+    const JoinedGroupList = GROUPMOCK.map( group => <GroupCard group={group} key={group.id} />);
+    const FoundGroupList = GROUPMOCK.map( group => <GroupCard group={group} key={group.id} />);
     return (
         <>
         {openModal && <CreateGroupModal closeModal={setOpenModal} />}
         <div className="max-size white-card">
             
-            {/* <div className='groupspage'>
-                <button onClick={() => handleClick()}>Create Group</button>
-            </div> */}
             < Tabs NameTab1="Joined Groups" NameTab2="Find Groups" ShowTab1={showMyGroups} setShowTab1={setShowMyGroups} ShowTab2={showFindGroups} setShowTab2={setShowFindGroups} / >
             <div className="flex justify-spacebetween align-center search-section">
                 <div className='SearchBar'>
@@ -26,12 +28,26 @@ export default function Groups() {
                 <i className="material-icons filter font">tune</i>
             </div>
             
-            <div className="create-group-button flex align-center justify-center" onClick={() => handleCreateClick()}>
-                <div className="flex justify-center align-center font eaves book fs18 canary-dark">
-                    <i className="material-icons filter">add</i>
-                    <span> Create a group</span>
+            {showMyGroups && 
+                <div className="create-group-button flex align-center justify-center" onClick={() => handleCreateClick()}>
+                    <div className="flex justify-center align-center font eaves book fs18 canary-dark">
+                        <i className="material-icons filter">add</i>
+                        <span> Create a group</span>
+                    </div>
                 </div>
-            </div>
+            }
+            
+
+            {showMyGroups && 
+                <div className="groups-list">
+                    {JoinedGroupList}
+                </div>
+            }
+            {showFindGroups && 
+                <div className="groups-list">
+                    {FoundGroupList}
+                </div>
+            }
         </div>
         
         </>
