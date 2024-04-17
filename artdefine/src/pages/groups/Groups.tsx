e2d30c8cddde790ce5538c3e63d120a3d12613f7
Charlotte-Overeem <r0781396@student.thomasmore.be>
2024-04-17 14:17:13 +0200

    added: scroll

--- artdefine/src/pages/groups/Groups.tsx
+++ artdefine/src/pages/groups/Groups.tsx
@@ -41,6 +41,9 @@ export default function Groups() {
             {showMyGroups && 
                 <div className="groups-list">
                     {JoinedGroupList}
+                    {JoinedGroupList}
+                    {JoinedGroupList}
+                    {JoinedGroupList}
                 </div>
             }
             {showFindGroups && 
