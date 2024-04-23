e6ef0a65dadced28dbab9007329593c8bba9e832
Charlotte-Overeem <r0781396@student.thomasmore.be>
2024-04-23 13:46:24 +0200

    added: dropdown component

---
+++ artdefine/src/components/dropdown.tsx
@@ -0,0 +1,58 @@
+import { useNavigate } from "react-router-dom";
+import { DropdownButtonModel } from "../model/DropdownButtonsModel";
+
+const Dropdown: React.FC<{ openDropdown: React.Dispatch<React.SetStateAction<boolean>>; buttonModels: DropdownButtonModel[]; }> = ({ openDropdown, buttonModels }) => {
+    // const navigate = useNavigate();
+    // const buttons: DropdownButtonModel[] = [
+    //     {
+    //         divider:false,
+    //         class: "navigate",
+    //         text: "test btn 1",
+    //         icon: "pets",
+    //         function: () => {navigate("/test")}
+    //     },
+    //     {
+    //         divider: true,
+    //     },
+    //     {
+    //         divider:false,
+    //         class: "link",
+    //         text: "test btn 2",
+    //         icon: "grade",
+    //         function: () => {navigate("/test2"); console.log("testing");}
+    //     }
+    // ]
+    
+
+    const buttonList = buttonModels.map(values => {
+        if (values.divider === true) {
+            return <div className="divider red"></div>
+        }else{
+            return <button className={"dropdown-btn flex justify-spacebetween align-center " + values.class} onClick={values.function}><span>{values.text}</span><i className="material-icons">{values.icon}</i></button> 
+        }
+    })
+    
+    return (
+        <>
+        <div
+            className="dropdownbackground"
+            onClick={() => {
+                openDropdown(false);
+                document.body.classList.remove("no-scroll");
+            }}
+        >
+
+        </div>
+        <div className="dropdown position absolute">
+            {buttonList}
+            {/* <button className="dropdown-btn flex justify-spacebetween align-center"><span>test btn</span><i className="material-icons">done</i></button>
+            <button className="dropdown-btn flex justify-spacebetween align-center"><span>test btn</span><i className="material-icons">done</i></button>
+            <div className="divider red"></div>
+            <button className="dropdown-btn flex justify-spacebetween align-center"><span>test btn</span><i className="material-icons">done</i></button> */}
+        </div>
+        </>
+      
+    );
+  };
+  
+  export default Dropdown;
---
+++ artdefine/src/model/DropdownButtonsModel.ts
@@ -0,0 +1,7 @@
+export interface DropdownButtonModel{
+    class?: string;
+    text?: string;
+    icon?: string;
+    divider: boolean;
+    function?: any;
+}
