7b0c2e7289b52d4dcb1906210384b012e7ebd548
Charlotte-Overeem <r0781396@student.thomasmore.be>
2024-05-12 15:45:59 +0200

    added: profile basic gallery card

--- artdefine/src/components/profile/Profile-home.tsx
+++ artdefine/src/components/profile/Profile-home.tsx
@@ -1,13 +1,85 @@
+import { useNavigate } from "react-router-dom";
 import { User } from "../../model/userModel";
+import Card from "../cards/Card";
 import TextCard from "../cards/TextCard";
+import { useEffect } from "react";
+import { Folder } from "../../model/FolderModel";
+import placeholder from "../../assets/images/mock-banner-pic.png";
 
-const ProfileHome: React.FC<{ rank: number, user?: User|null }> = ({ rank, user }) => {
-  
+const ProfileHome: React.FC<{ rank: number, user?: User|null, handleCategoryButtonClick:any }> = ({ rank, user, handleCategoryButtonClick }) => {
+    //TODO: one image from the folder as tumbnail
+
+    function compare(a:Folder,b:Folder) {
+        if (a.folder_order < b.folder_order)
+           return -1;
+        if (a.folder_order > b.folder_order)
+          return 1;
+        return 0;
+      }
+
+    useEffect(() => {
+        if (user) {
+            user.folders.sort(compare);
+
+            
+        }
+      }, []);
+    
+
+    const moreClickHandler = () => {
+        handleCategoryButtonClick('gallery');
+    }
+
     return (
         <>
         {user?
            <div className="profile-home">
                 <TextCard title={user.user_name}  text={user.user_bio} subTitle={user.user_subtitle} links={user.links} pronouns={user.user_pronouns} isUser={true} creationDate={user.user_creationdate} />
+                <Card
+                    title={"Gallery"}
+                    isUser={false}
+                    cssProperty="gallery-tumbnail"
+                    hasMore={true} 
+                    moreClickHandler={moreClickHandler}
+                    content={
+                        <div className="flex justify-spacebetween">
+                        {user.folders.length === 3?
+                        <><div onClick={() => {console.log("navigate to gallery & select folder")}} key={user.folders[0].id} className="clickable foldercard flex direction-column direction-column align-center justify-spacebetween">
+                        <img src={placeholder} alt="" />
+                        <div>{user.folders[0].folder_name}</div>
+                        </div>
+                        <div onClick={() => {console.log("navigate to gallery & select folder")}} key={user.folders[1].id} className="clickable foldercard flex direction-column direction-column align-center justify-spacebetween">
+                        <img src={placeholder} alt="" />
+                        <div>{user.folders[1].folder_name}</div>
+                        </div>
+                        <div onClick={() => {console.log("navigate to gallery & select folder")}} key={user.folders[2].id} className="clickable foldercard flex direction-column direction-column align-center justify-spacebetween">
+                        <img src={placeholder} alt="" />
+                        <div>{user.folders[2].folder_name}</div>
+                        </div></>
+                        :user.folders.length === 2?
+                        <><div onClick={() => {console.log("navigate to gallery & select folder")}} key={user.folders[0].id} className="clickable foldercard flex direction-column direction-column align-center justify-spacebetween">
+                        <img src={placeholder} alt="" />
+                        <div>{user.folders[0].folder_name}</div>
+                        </div>
+                        <div onClick={() => {console.log("navigate to gallery & select folder")}} key={user.folders[1].id} className="clickable foldercard flex direction-column direction-column align-center justify-spacebetween">
+                        <img src={placeholder} alt="" />
+                        <div>{user.folders[1].folder_name}</div>
+                        </div></>
+                        :user.folders.length === 1?
+                        <><div onClick={() => {console.log("navigate to gallery & select folder")}} key={user.folders[0].id} className="clickable foldercard flex direction-column direction-column align-center justify-spacebetween">
+                        <img src={placeholder} alt="" />
+                        <div>{user.folders[0].folder_name}</div>
+                        </div></>
+                        :
+                        "Folders couldn't be loaded"
+                    }
+                        
+                        </div>
+                        
+                        
+                    }
+                />
+                
             </div> 
             :
             ''
--- artdefine/src/pages/Profile/Profile.tsx
+++ artdefine/src/pages/Profile/Profile.tsx
@@ -44,7 +44,7 @@ export default function Profile() {
         <>
         <div className="page profile own-profile">
             <ProfileMain rank={1} profileActive={profileActive} groupsActive={groupsActive} galleryActive={galleryActive} handleCategoryButtonClick={handleCategoryButtonClick} />
-            {profileActive && !galleryActive && !groupsActive? <ProfileHome rank={1} user={user}/>
+            {profileActive && !galleryActive && !groupsActive? <ProfileHome rank={1} user={user} handleCategoryButtonClick={handleCategoryButtonClick}/>
             :galleryActive && !profileActive && !groupsActive? <ProfileGallery rank={1} folders={user?.folders}/>
             :groupsActive && !profileActive && !galleryActive? <ProfileGroups rank={1}/>
             : <p>Something went wrong - please reload the page</p>}
