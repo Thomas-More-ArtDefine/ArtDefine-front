2d2798337f32eb3426a6c3a91486e71f822f3120
Dylan <79404455+DylanBracke@users.noreply.github.com>
2024-05-18 10:55:56 +0200

    small http change

--- artdefine/src/api/http-common.ts
+++ artdefine/src/api/http-common.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 const { REACT_APP_API_URL} = process.env;
 const api = axios.create({
-  baseURL: `https://${REACT_APP_API_URL}`,
+  baseURL: `http://${REACT_APP_API_URL}`,
   headers: {
     "Content-type": "application/json"
   }
